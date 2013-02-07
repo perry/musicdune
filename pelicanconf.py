cb93aac5c70c3611115e6d0f04868f4980b35922
Perry Roper <perryroper@gmail.com>
2013-02-07 20:25:53 +0000

    After pelican-quickstart

---
+++ pelicanconf.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'Perry'
+SITENAME = u'MusicDune'
+SITEURL = ''
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = u'en'
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10

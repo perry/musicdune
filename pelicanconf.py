c1d617de027043d82d198457b339fc8955d17375
Perry Roper <perryroper@gmail.com>
2013-02-10 12:25:20 +0000

    Define URL + SAVE_AS

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,3 +16,18 @@ THEME = 'themes/musicdune'
 PLUGINS = ['plugins.assets']
 
 DEFAULT_PAGINATION = 10
+
+ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+
+AUTHOR_URL = 'author/{name}/'
+AUTHOR_SAVE_AS = 'author/{name}/index.html'
+
+CATEGORY_URL = 'category/{name}/'
+CATEGORY_SAVE_AS = 'category/{name}/index.html'
+
+TAG_URL = 'tag/{name}/'
+TAG_SAVE_AS = 'tag/{name}/index.html'

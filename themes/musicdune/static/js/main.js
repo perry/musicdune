cee5d6f7f7c38ecbd15c7f486d07797768432531
Perry Roper <perryroper@gmail.com>
2013-03-08 20:01:13 +0000

    Use // for comments in main.js

--- themes/musicdune/static/js/main.js
+++ themes/musicdune/static/js/main.js
@@ -1,7 +1,7 @@
-/* Prevent links in 'standalone' iOS apps opening in Safari */
+// Prevent links in 'standalone' iOS apps opening in Safari
 (function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(d.href.indexOf("http")||~d.href.indexOf(e.host))&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone");
 
-/*! Normalized address bar hiding for iOS & Android (c) @scottjehl MIT License */
+// ! Normalized address bar hiding for iOS & Android (c) @scottjehl MIT License
 (function( win ){
     var doc = win.document;
 
